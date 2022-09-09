32e46570939c87c6f5963e6106a5af2e4b4789bd
dalisodaka <dalisodaka@hotmail.com>
2022-09-09 09:51:39 -0700

    my commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+
 /* more headers goes there */
 /* betty style doc for function main goes there */
 int main(void)
@@ -8,6 +9,6 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+/* your code goes there */
 	return (0);
 }

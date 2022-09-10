57d9bdbf66a8609beaf9f822388a76560ac3d76c
dalisodaka <dalisodaka@hotmail.com>
2022-09-09 20:57:02 -0700

    My last commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - Prints out the last part of a quote in the standard error.
+ *
+ * Return: Always (1) for error
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, \ 2015-10-19\n", 59);
+return (1);
+}

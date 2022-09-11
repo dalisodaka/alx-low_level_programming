59835460fdccf20b0da06a667d6b4aea98ca95c2
dalisodaka <dalisodaka@hotmail.com>
2022-09-11 11:08:04 -0700

    My 100 print commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 /**
- * main - print numbers 00 to 99
+ * main - print combo of 2 numbers
  * description - nested loop
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int a;
-
-	for (a = 0; a < 100; a++)
+	int tens;
+	int ones;
+	for (tens = 0; tens <= 9; tens++)
 	{
-		putchar('0' + a / 10);
-		putchar('0' + a % 10);
-		if (!(a == 99))
+		for (ones = tens + 1; ones <= 9; ones++)
 		{
-			putchar(',');
-			putchar(' ');
+			putchar(tens + '0');
+			putchar(ones + '0');
+			if (tens < 8)
+			{
+				putchar (',');
+				putchar (' ');
+			}
 		}
 	}
 	putchar('\n');

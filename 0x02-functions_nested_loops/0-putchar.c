f656109c0b8bb097ad22f801cf8b366f80208bb9
dalisodaka <dalisodaka@hotmail.com>
2022-09-13 07:50:02 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "_putchar"
+/**
+ * main - prints Holberton
+ *
+ * Return: Always 0 (Success)
+ */int main(void)
+{
+	int i = 0;
+	char s[] = "_putchar\n";
+	while (s[i])
+	{
+		_putchar (s[i]);
+		i++;
+	}
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This file is all about C programmming, functions, nested and loops

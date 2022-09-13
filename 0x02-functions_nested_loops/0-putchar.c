2f9f6be68758631d26c51112be293de579cc2bc0
dalisodaka <dalisodaka@hotmail.com>
2022-09-13 15:16:27 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,19 @@
-#include "_putchar"
+#include "main.h"
 /**
- * main - prints Holberton
- *
- * Return: Always 0 (Success)
- */int main(void)
+ * main - prints putchar
+ * Return: always 0
+ */
+int main(void)
 {
-	int i = 0;
-	char s[] = "_putchar\n";
-	while (s[i])
-	{
-		_putchar (s[i]);
-		i++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }

5a2613c12cf1d562ec7f4104fab1a9d947622b22
dalisodaka <dalisodaka@hotmail.com>
2022-09-14 13:18:53 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet - function to print abc
- *
- * Return: 0
+ *print_alphabet - function to print abc
+ *Return: 0
  */
 void print_alphabet(void)
 {
@@ -13,5 +12,5 @@ void print_alphabet(void)
 	{
 		_putchar(c);
 	}
-_putchar('\n');
+	_putchar('\n');
 }

c28831370b37317e16a2e6449bded9da10c27982
alephkat <alphamuseka@gmail.com>
2022-09-23 11:15:18 +0200

    Task 13 commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,9 +1,7 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
   * print_number - Prints any integer with putchar
   * @n: Number to prints
-  *
   * Return: Nothing
   */
 void print_number(int n)

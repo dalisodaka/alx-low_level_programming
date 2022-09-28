509e29bdcb73564f8545c3c004ab01c80b6a09b7
dalisodaka <dalisodaka1990@gmail.com>
2022-09-28 11:18:59 -0700

    Task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,13 +1,14 @@
-/*                                                               * File: 1-print_rev_recursion.c                                 * Auth: daliso                                                  */                                                             #include "main.h"
-/**
- * _print_rev_recursion - Prints a string in reverse.            * @s: The string to be printed.                                 */
-void _print_rev_recursion(char *s)
+#include "main.h"
 
-}
-	if (*s)
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: the string to reverse
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
-

406299116faad6d789d88b06b324f2cca15f1d1f
dalisodaka <dalisodaka1990@gmail.com>
2022-10-04 15:28:53 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -5,13 +5,16 @@
  * str_concat - concatenates two strings into a newly allocated space in memory
  * @s1: First string to be concatenated
  * @s2: Second string to be concatenated after s1
+ *
  * Return: A pointer to the newly allocated memory, or NULL on failure
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	chr *concat_str;
-	int index, concat_index = 0, len = 0;
+	int i;
+	char *ptr = NULL;
+	char *as1 = s1;
+	char *as2 = s2;
 
 	if (as1 == NULL)
 		as1 = "";
@@ -19,19 +22,32 @@ char *str_concat(char *s1, char *s2)
 	if (as2 == NULL)
 		as2 = "";
 
-	for (index = 0; s1[index] || s2[index]; index++)
-		len++;
-	concat_str = malloc(sizeof(char) * len);
+	ptr = as1;
+	while (*ptr++)
+		i++;
 
-	if (concat_str == NULL)
+
+
+	ptr = as2;
+	while (*ptr++)
+		i++;
+
+	i++;
+	ptr = malloc(i * sizeof(char));
+
+	if (ptr == NULL)
 		return (NULL);
 
-	for (index = 0; s1[index]; index++)
-		concat_str[concat_index++] = s1[index];
+	i = 0;
+	while (*as1)
+	{
+		ptr[i++] = *as1++;
+	}
+	while (*as2)
+	{
+		ptr[i++] = *as2++;
+	}
+	ptr[i] = *as2;
 
-	for (index = 0; s2[index]; index++)
-		concat_str[concat_index++] = s2[index];
-
-	return (concat_str);
-
+	return (ptr);
 }
